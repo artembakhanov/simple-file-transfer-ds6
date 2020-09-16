f59cd4c8402da1b373c70c339b2fda3223bff853
Artem Bakhanov <artembakhanov@gmail.com>
2020-09-17 01:00:33 +0300

    Init commit

---
+++ client.py
@@ -0,0 +1,124 @@
+import argparse
+import json
+import os
+import ntpath
+import socket
+import re
+import time
+import signal
+
+# Constants
+HOST_REGEX = "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$"
+PORT_REGEX = "^()([1-9]|[1-5]?[0-9]{2,4}|6[1-4][0-9]{3}|65[1-4][0-9]{2}|655[1-2][0-9]|6553[1-5])$"
+RED = "\u001b[31;1m"
+GREEN = "\u001b[32;1m"
+YELLOW = "\u001b[33;1m"
+RESET = "\u001b[0m"
+BUFF_SIZE = 2048
+
+
+def signal_handler(sig, frame):
+    try:
+        global sock
+        sock.close()
+    except:
+        pass
+    exit(0)
+
+
+signal.signal(signal.SIGHUP, signal_handler)
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGQUIT, signal_handler)
+
+
+# Progress Class
+class Progress:
+    message = f"\r{YELLOW}{{name}} |{{bar}}| Progress: {{progress:.2f}}%{RESET}"
+    bar_size = 25
+    update_eta_rate = 100
+
+    def __init__(self, total, progress_name):
+        if total <= 0:
+            raise ValueError("total should be more than 0")
+
+        self.last_time = [time.time()] * self.update_eta_rate
+        self.total = total
+        self.progress = 0
+        self.progress_name = progress_name
+        self.eta_counter = -1
+        print(
+            self.message.format(name=self.progress_name, bar='█' * 0 + ' ' * self.bar_size, progress=0),
+            end='')
+
+    def update(self, new_progress):
+        self.eta_counter = (self.eta_counter + 1) % self.update_eta_rate
+        now = time.time()
+        delta = now - self.last_time[self.eta_counter]
+        self.last_time[self.eta_counter] = now
+        self.progress += new_progress
+
+        bars = int(self.progress / self.total * self.bar_size)
+        spaces = self.bar_size - bars
+        print(self.message.format(name=self.progress_name,
+                                  bar='█' * bars + " " * spaces,
+                                  progress=min(100, (self.progress / self.total) * 100)), end='')
+        if self.progress >= self.total:
+            print(f"\r{GREEN}{self.progress_name} done!{RESET}\033[K")
+
+
+# Parse arguments
+parser = argparse.ArgumentParser(description="Client script for sending files to the server.\n"
+                                             "Made by Artem Bakhanov as a DS lab assignment.")
+
+parser.add_argument("file_name", help="path to the file to send", )
+parser.add_argument("host", help="ip address or host address of the server")
+parser.add_argument("port", help="port of the server script", type=int)
+
+# Check arguments
+args = parser.parse_args()
+
+if not os.path.isfile(args.file_name):
+    parser.error(f"{RED}File does not exist or file path is wrong{RESET}")
+
+if not re.match(HOST_REGEX, args.host):
+    parser.error(f"{RED}Not valid host name{RESET}")
+
+if not re.match(PORT_REGEX, str(args.port)):
+    parser.error(f"{RED}Not valid port{RESET}")
+
+# Get information
+file_name = args.file_name
+file_size = os.path.getsize(file_name)
+host = args.host
+port = args.port
+
+# get the base name
+# it will send file name without folders (directories)
+file_basename = ntpath.basename(args.file_name)
+
+# Create socket
+print(f"{YELLOW}Connecting to {host}:{port}{RESET}", end='')
+sock = socket.socket()
+sock.connect((host, port))
+print(f"\r{GREEN}Connected to {host}:{port}!{RESET}")
+
+# Start transmitting file
+print(f"{YELLOW}Sending file info...{RESET}", end='')
+sock.sendall(f'{{"file_name": "{file_basename}", "size": {file_size}}}'.encode())
+file_info = json.loads(sock.recv(BUFF_SIZE).decode())
+print(f"\r{GREEN}File name on server: {file_info['server_file_name']}.{RESET}\n"
+      f"{YELLOW}Starting transmitting the file...")
+
+# create progress bar
+progress = Progress(file_size, "Sending file")
+
+# send file
+with open(file_name, 'rb') as f:
+    payload = f.read(BUFF_SIZE)
+    while payload:
+        sock.send(payload)
+        progress.update(BUFF_SIZE)
+        payload = f.read(BUFF_SIZE)
+print(f"\033[F\033[F{GREEN}Finished{RESET}\033[K")
+sock.close()
+print(f"{GREEN}Connection closed{RESET}")
---
+++ server.py
@@ -0,0 +1,170 @@
+import json
+import os
+import types
+import socket
+import selectors
+from enum import Enum
+from threading import Thread
+import glob
+
+# Constants
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 8800
+BUFFER_SIZE = 2048
+DIRECTORY = "./server_files"
+META = "./server_meta"
+RED = "\u001b[31;1m"
+GREEN = "\u001b[32;1m"
+YELLOW = "\u001b[33;1m"
+RESET = "\u001b[0m"
+
+
+class ClientState(Enum):
+    """
+    Client state. Used for selector.
+    """
+    NEW = (0, True)
+    GOT_FILE_NAME = (1, False)
+    TRANSMITTING = (2, True)
+    FINISHED_TRANSMITTING = (3, False)
+
+
+clients = {}
+
+
+def remove_client(addr):
+    """Remove a client from a list"""
+    global clients
+    del clients[addr]
+
+
+class Conductor(Thread):
+    def __init__(self, selector):
+        super().__init__(daemon=True)
+        self.selector = selector
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def run(self):
+        next_name = 1
+
+        self.sock.bind((SERVER_HOST, SERVER_PORT))
+        self.sock.listen()
+        while True:
+            con, addr = self.sock.accept()
+
+            # not blocking
+            con.setblocking(False)
+            # new client - addr - unique identifier
+            client = types.SimpleNamespace(addr=addr, outb=b'', state=ClientState.NEW, file=None, file_size=None,
+                                           file_got=0, retry=0)
+            clients[addr] = client
+            print(f"{GREEN}[+] {client.addr} - Connected{RESET}")
+
+            events = selectors.EVENT_READ | selectors.EVENT_WRITE
+            # register this socket to selector for both READ and WRITE events
+            self.selector.register(con, events, data=client)
+
+
+class MultiClientServer(Thread):
+    def __init__(self, selector):
+        super().__init__(daemon=True)
+        self.selector = selector
+
+    def _close(self, sock, client):
+        client.file.close()
+        remove_client(client.addr)
+        # don't forget to unregister this socket
+        self.selector.unregister(sock)
+        sock.close()
+        print(f"{GREEN}[-] {client.addr} - Disconnected{RESET}")
+
+    def _read(self, sock, client):
+        if client.state == ClientState.NEW:
+            self._get_file_info(sock, client)
+        elif client.state == ClientState.TRANSMITTING:
+            self._get_file_part(sock, client)
+
+    def _write(self, sock, client):
+        if client.state == ClientState.GOT_FILE_NAME:
+            sent = sock.send(client.outb)
+            client.state = ClientState.TRANSMITTING
+
+    def run(self):
+        while True:
+            # get sockets which are ready for smth (READ or WRITE)
+            events = self.selector.select(timeout=None)
+            for key, mask in events:
+                sock = key.fileobj
+                client = key.data
+                # if there is smth to READ
+                if mask & selectors.EVENT_READ and client.state.value[1]:
+                    # print(client, "ready to read")
+                    self._read(sock, client)
+                # if the client ready to recive some data
+                if mask & selectors.EVENT_WRITE and not client.state.value[1]:
+                    # print(client, "ready to write")
+                    self._write(sock, client)
+
+    def _get_file_info(self, sock, client):
+        data = sock.recv(BUFFER_SIZE)
+
+        # get file information
+        file_info = json.loads(data.decode())
+        file_name = file_info['file_name']
+
+        file_name_parts = file_name.rsplit(".", 1)
+
+        # for each file there is a meta file
+        # it stores the counter
+        meta_file = f"{META}/{file_name}.meta"
+        if not os.path.isfile(meta_file):
+            # file is new
+            file_counter = 0
+            server_file_name = file_name
+        else:
+            with open(meta_file) as f:
+                file_counter = int(f.read())
+            server_file_name = f"{file_name_parts[0]}_copy{file_counter}.{file_name_parts[1]}"
+        with open(meta_file, 'w') as f:
+            f.write(str(file_counter + 1))
+
+        # answer to the client that the operation is successful
+        client.outb = json.dumps({"success": True, "copy": file_counter != 1, "server_file_name": server_file_name,
+                                  "message": "If you see this message everything is fine:)"}).encode()
+        client.file = open(f"{DIRECTORY}/{server_file_name}", "wb")
+        client.state = ClientState.GOT_FILE_NAME
+        client.file_size = file_info['size']
+        client.got = 0
+
+        print(f"{GREEN}[i] {client.addr} - New file: {file_name}:{server_file_name}{RESET}")
+
+    def _get_file_part(self, sock, client):
+        if client.got >= client.file_size:
+            self._close(sock, client)
+            return
+        data = sock.recv(BUFFER_SIZE)
+        if not data:
+            if client.retry > 10:
+                # most probably the client is down
+                self._close(sock, client)
+                return
+            # give a chance to the client
+            print(f"{RED}[!] {client.addr} - data packet is empty; retries left {10 - client.retry}{RESET}")
+            client.retry += 1
+            return
+        client.retry = 0
+        client.got += BUFFER_SIZE
+        client.file.write(data)
+
+
+def main():
+    sel = selectors.DefaultSelector()
+    MultiClientServer(sel).start()
+    conductor = Conductor(sel)
+    conductor.start()
+    conductor.join()
+
+
+if __name__ == "__main__":
+    main()
